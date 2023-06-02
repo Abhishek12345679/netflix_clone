d9c05f6e1d42bbc997eb5408b8126f394e3206f6
Abhishek12345679 <sah755146@gmail.com>
2023-06-02 17:06:39 +0530

    added horizontal list view inside single child scroll view

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -11,6 +11,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<Profile> profiles = [
+    Profile(
+        'https://www.rainforest-alliance.org/wp-content/uploads/2021/06/capybara-square-1-400x400.jpg.webp',
+        "OK I PULL UP"),
+    Profile(
+        'https://www.videomeme.in/wp-content/uploads/2023/04/happy-happy-happy-cat-1.jpg',
+        "UR MOM MF"),
+    Profile(
+        'https://www.rainforest-alliance.org/wp-content/uploads/2021/06/capybara-square-1-400x400.jpg.webp',
+        "OK I PULL UP"),
+    Profile(
+        'https://www.videomeme.in/wp-content/uploads/2023/04/happy-happy-happy-cat-1.jpg',
+        "UR MOM MF"),
+    Profile(
+        'https://www.rainforest-alliance.org/wp-content/uploads/2021/06/capybara-square-1-400x400.jpg.webp',
+        "OK I PULL UP"),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +58,44 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Center(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(20.0),
+              width: double.infinity,
+              height: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: Colors.red[500],
+              ),
+            ),
+            SizedBox(
+              height: 175,
+              width: double.infinity,
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  final currentProfile = profiles.elementAt(index);
+                  return Container(
+                    margin: EdgeInsets.only(right: 20),
+                    height: 100,
+                    width: 125,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(widget.profile.profilePicture),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  );
+                },
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
